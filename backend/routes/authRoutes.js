e7f342c4df8c9a13b7698ddd472f40ab6b838301
Jaya Jha <jayajha122006@gmail.com>
2025-12-19 20:45:30 +0530

    fixed reload issue

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 // Schema for Registration
 const registerSchema = z.object({
     username: z.string().min(3, "Username must be at least 3 characters long"),
-    email: z.string().email("Invalid email address format"),
+    email: z.email("Invalid email address format"),
     password: z.string().min(6, "Password must be at least 6 characters long")
 });
 
